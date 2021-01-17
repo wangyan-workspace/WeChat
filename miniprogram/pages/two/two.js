a1f478ed9a5265659e64f17307cfde32cc2a895e
wangyan-workspace <2107014530@qq.com>
2021-01-17 16:50:04 +0800

    微信小程序引用组件库

--- miniprogram/pages/two/two.js
+++ miniprogram/pages/two/two.js
@@ -29,7 +29,20 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.request({
+      url: 'https://m.douban.com/rexxar/api/v2/skynet/playlists?from_rexxar=true', //仅为示例，并非真实的接口地址
+      data: {
+        x: '',
+        y: ''
+      },
+      //请求头
+      // header: {
+      //   'content-type': 'application/json' // 默认值
+      // },
+      success (res) {
+        console.log(res.data)
+      }
+    })
   },
 
   /**
